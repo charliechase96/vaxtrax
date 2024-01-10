dcc9570078423bbfeb47cba233e7bf865280c2c9
Charlie Gray <charliechasegray@gmail.com>
2024-01-10 01:57:08 -0800

    change dropdown to parseable values

--- src/Components/Alerts/AlertForm.js
+++ src/Components/Alerts/AlertForm.js
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 
 function AlertForm() {
-    const [alertDay, setAlertDay] = useState("Set alert for...");
+    const [alertDay, setAlertDay] = useState("##");
+
+    function handleSubmit() {
+        
+    }
 
     return (
         <div className="alert-form">
-            <form>
-                <select>
+            <form onSubmit={handleSubmit}>
+                <select 
+                    value={alertDay}
+                    onChange={(e) => setAlertDay(e.target.value)}
+                >
                     <option 
                         disabled 
-                        value="Set alert for..."
+                        value="##"
                     >
                         Set alert for...
                     </option>
-                    <option value="60 days before">60 days before</option>
-                    <option value="30 days before">30 days before</option>
-                    <option value="7 days before">7 days before</option>
-                    <option value="On due date">On due date</option>
+                    <option value="60">60</option>
+                    <option value="30">30</option>
+                    <option value="7">7</option>
+                    <option value="0">0</option>
                 </select>
+                <p>days before due</p>
                 <button>Set Alert</button>
             </form>
         </div>
