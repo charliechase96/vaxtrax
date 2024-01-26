4109a9cd7d8f0410970eb1fc95f72cb046f90b94
Charlie Gray <charliechasegray@gmail.com>
2024-01-26 11:12:14 -0700

    drop all tables

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://charlie_chase_gray:BLONDIE_96@localhost/vaxtrax_db'
+    SQLALCHEMY_DATABASE_URI = 'sqliteL///vaxtrax.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     JWT_SECRET_KEY = os.urandom(24)
