607b8c056a8ffd844ce0dedef518d098509a02fa
Charlie Gray <charliechasegray@gmail.com>
2024-01-26 12:03:20 -0700

    set database uri

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqliteL///vaxtrax.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     JWT_SECRET_KEY = os.urandom(24)
