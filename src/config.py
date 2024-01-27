ddea98ff32645f4a27b9fb3d7483a9678c059970
Charlie Gray <charliechasegray@gmail.com>
2024-01-27 05:49:32 -0700

    fix and set environment variable

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     JWT_SECRET_KEY = os.urandom(24)
